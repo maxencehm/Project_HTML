71ef5e58017285ad98a56c8e770118ca5c0a688e
maxencehm <157626014+maxencehm@users.noreply.github.com>
2024-02-07 09:33:07 +0000

    advancement

--- snake.js
+++ snake.js
@@ -1,11 +1,35 @@
-function snacke(){
-    var canvas=document.getElementById("main-snake")
-    var g = canvas.getContext("2d")
-    var colors = {216:175};                                                     // couleurs des careau
+var size = 50;
+var coordonee_pomme_x = 12;
+var coordonee_pomme_y = 7;
+let body = [[1,7],[2,7],[3,7]] ;
+function snake(){
+  var canvas=document.getElementById("main-snake");
+ 
+  ctx = canvas.getContext('2d');
+    let colors = ["rgba(61,128,58)","rgba(61,175,58)"];                                                     // couleurs des careau
+    globalThis.ctx;
     for (var i=0;i<15;i++){                                                       // affichage cadrillage
+
       for (var j = 0;j<15;j++){
-          g2d.setColor(new Color(61,colors[(i+j)%2],58));
-          g2d.fillRect(size*(i+8),size*(j+1),size, size);
+          ctx.fillStyle = colors[(i+j)%2];
+          ctx.fillRect(size*(i),size*(j),size, size);
       }
     }
+    affiche_pomme();
+}
+function affiche_pomme(){
+  ctx.beginPath();
+  ctx.arc(coordonee_pomme_x*50 +26, coordonee_pomme_y*50+25, 25, 0, 2 * Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  var p = document.getElementById("1")
+  p.innerHTML = "ca marche"
+}
+function affiche_snake(){
+  for (var part in body){
+    var i=part[0];
+    var j=part[1];
+    ctx.fillStyle = "blue";
+    ctx.fillRect(size*(i)-1,size*(j)-1,size-1, size-1);
+  }
 }
