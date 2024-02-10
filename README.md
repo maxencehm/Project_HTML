fdecad18291040b7959c026046a2f540050a49ef
maxencehm <157626014+maxencehm@users.noreply.github.com>
2024-02-10 08:46:11 +0000

    boutons fonctionnel + corp du snake + reorga du css

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 https://prod.liveshare.vsengsaas.visualstudio.com/join?834D4688C24A0FEAD4E77A226E69003084E4
-sdqsd
+il faudrait que tu choisisse les couleur pour les header des trois jeux qu'il reste à faire
--- snake.js
+++ snake.js
@@ -4,31 +4,37 @@ var coordonee_pomme_y = 7;
 let body = [[1,7],[2,7],[3,7]] ;
 function snake(){
   var canvas=document.getElementById("main-snake");
- 
   ctx = canvas.getContext('2d');
-    let colors = ["rgba(61,128,58)","rgba(61,175,58)"];                                                     // couleurs des careau
-    globalThis.ctx;
-    for (var i=0;i<15;i++){                                                       // affichage cadrillage
+  globalThis.ctx;
+  affiche_cadrillage();
+  affiche_pomme();
+  affiche_snake();
 
-      for (var j = 0;j<15;j++){
-          ctx.fillStyle = colors[(i+j)%2];
-          ctx.fillRect(size*(i),size*(j),size, size);
-      }
+}
+function affiche_cadrillage(){
+  let colors = ["rgba(61,128,58)","rgba(61,175,58)"];                                                     // couleurs des careau
+  for (var i=0;i<15;i++){                                                       // affichage cadrillage
+    
+    for (var j = 0;j<15;j++){
+      ctx.fillStyle = colors[(i+j)%2];
+      ctx.fillRect(size*(i),size*(j),size, size);
     }
-    affiche_pomme();
+  }
+  
 }
 function affiche_pomme(){
   ctx.beginPath();
   ctx.arc(coordonee_pomme_x*50 +26, coordonee_pomme_y*50+25, 25, 0, 2 * Math.PI);
   ctx.fillStyle = "red";
+
   ctx.fill();
-  var p = document.getElementById("1")
-  p.innerHTML = "ca marche"
 }
 function affiche_snake(){
-  for (var part in body){
-    var i=part[0];
-    var j=part[1];
+  for (let part in body){
+    var p = document.getElementById("1");
+    p.innerHTML = part;
+    var i=body[part][0];
+    var j=body[part][1];
     ctx.fillStyle = "blue";
     ctx.fillRect(size*(i)-1,size*(j)-1,size-1, size-1);
   }
