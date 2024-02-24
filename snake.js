6a382d25a025a521591d88223ce04c2ee4fbce3c
maxencehm <157626014+maxencehm@users.noreply.github.com>
2024-02-24 21:42:50 +0000

    modif snake starting + css useless remove

--- snake.js
+++ snake.js
@@ -1,59 +1,67 @@
-var size = 50;
-var coordonee_pomme_x = 12;
-var coordonee_pomme_y = 7;
-let body = [[1,7],[2,7],[3,7]];
-var direction = "E";
-let head =[3,7];
-let cadrillage = new Array(15);
-var game =true
-var score =0;
-var time =Math.floor(Date.now()/150);
-var jeu =1;
-var direction_precedente;
-var canvas=document.getElementById("main-snake");
-var ctx = canvas.getContext('2d');
-affiche_cadrillage();
-var pomme = {
-  carreau_x : 12,
-  carreau_y :7,
-  draw:function(){
-    ctx.beginPath();
-    ctx.arc(this.carreau_x*size +26, this.carreau_y*size+25, 25, 0, 2 * Math.PI);
-    ctx.fillStyle = "red";
-    
-    ctx.fill();
-  },
-  change_location:function(){
-    this.carreau_x = Math.round(Math.random()*14);
-    this.carreau_y = Math.round(Math.random()*14);
-  },
-  pomme_in_body: function(){
-    for(var element in body){
-      if (body[element][0]==this.carreau_x && body[element][1]==this.carreau_y){
-        return true;
-      }
-    };
-    return false;
-  },
-  get_pomme:function(){
-    if (head[0]==pomme.carreau_x && head[1]==pomme.carreau_y){
-      score++;
-      document.getElementById("score").innerHTML = "score = "+score;
-      console.log(this.carreau_x,this.carreau_y)
-      while (pomme.pomme_in_body()){
-        pomme.change_location();
-      }
+
+function innit(){
+  size = 50;
+  coordonee_pomme_x = 12;
+  coordonee_pomme_y = 7;
+  body = [[1,7],[2,7],[3,7]];
+  direction = "E";
+  head =[3,7];
+  game =true
+  score =0;
+  time =Math.floor(Date.now()/150);
+  jeu =1;
+  direction_precedente= "E";
+  pomme = {
+    carreau_x : 12,
+    carreau_y :7,
+    draw:function(){
+      ctx.beginPath();
+      ctx.arc(this.carreau_x*size +26, this.carreau_y*size+25, 25, 0, 2 * Math.PI);
+      ctx.fillStyle = "red";
       
+      ctx.fill();
+    },
+    change_location:function(){
+      this.carreau_x = Math.round(Math.random()*14);
+      this.carreau_y = Math.round(Math.random()*14);
+    },
+    pomme_in_body: function(){
+      for(var element in body){
+        if (body[element][0]==this.carreau_x && body[element][1]==this.carreau_y){
+          return true;
+        }
+      };
+      return false;
+    },
+    get_pomme:function(){
+      if (head[0]==pomme.carreau_x && head[1]==pomme.carreau_y){
+        score++;
+        document.getElementById("score").innerHTML = "score = "+score;
+        console.log(this.carreau_x,this.carreau_y)
+        while (pomme.pomme_in_body()){
+          pomme.change_location();
+        }
+
+      }
+    }}
+    canvas=document.getElementById("main-snake");
+    ctx = canvas.getContext('2d');
+    affiche_cadrillage();
+    pomme.draw();
+    affiche_snake();
+    affiche_eyes();
+    ctx.font = "48px serif"
+    ctx.fillText("apuyez ici pour commencer à jouer", 30, 335)
+    onkeydown = (event)=>{
+      snake()
     }
-  }
-  
-  
 }
 function draw() {
   if (time!=Math.floor(Date.now()/150)){
     move();
     if (detect_colision()){
       ctx.fillText("GAME OVER", 230, 335);
+      game = false;
       return;
     }
     pomme.get_pomme();
@@ -67,21 +75,6 @@ function draw() {
   raf = window.requestAnimationFrame(draw);
   
 }
-pomme.draw();
-affiche_snake();
-affiche_eyes();
-ctx.font = "48px serif"
-ctx.fillText("apuyez ici pour commencer à jouer", 30, 335)
-for (var i=0;i<15;i++){
-  cadrillage[i]= new Array(15);
-  for (var j=0;j<15;j++){
-    cadrillage[i][j] = 0
-  }
-}
-for (let part in body){
-  cadrillage[body[part][1]][body[part][0]]=1;
-}
-cadrillage[coordonee_pomme_y][coordonee_pomme_x]=2
 function snake(){
   if(game){
     raf = window.requestAnimationFrame(draw);
@@ -102,9 +95,10 @@ function snake(){
       } }
     }
     else {
-      
+      innit()
       
     }
+    console.log("test")
   }
   
   function affiche_cadrillage(){
@@ -189,4 +183,3 @@ function snake(){
       }
       
     }
-    
