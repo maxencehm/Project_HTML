a08a31ef906b713440b9d768e780d068f5d6706d
Theotimm <158271626+Theotimm@users.noreply.github.com>
2024-03-10 17:02:03 +0100

    Add files via upload

---
+++ Chifoumi.js
@@ -0,0 +1,35 @@
+let wins= 0; let loses=0;
+const choices= ["rock","paper","scissors"];
+const gameStatus = document.getElementById("gameStatus");
+const gameScore = document.getElementById("gameScore");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+function runGame(userChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+   switch(userChoice + '_' + computerChoice) {
+    case 'rock_paper':
+    case 'paper_scissors':
+    case 'scissors_rock':
+        loses  += 1;
+        gameStatus.innerHTML= 'M: ${userChoice} | B: ${computerChoice} -> B Wins'
+        break;
+    case 'rock_scissors':
+    case 'paper_rock':
+    case 'scissors_paper':
+        wins  += 1;
+        gameStatus.innerHTML= 'M: ${userChoice} | B: ${computerChoice} -> M Wins'
+        break;
+    case 'rock_rock':
+    case 'paper_paper':
+    case 'scissors_scissors':
+        gameStatus.innerHTML= 'M: ${userChoice} | B: ${computerChoice} -> Egalité'
+        break;
+   }
+   gameScore.innerHTML = 'ME: ${wins} | Bot: ${loses}';
+}
+
+rock.addEventListener("click",()=>console.log("rock"));
+paper.addEventListener("click",()=>console.log("papier"));
+scissors.addEventListener("click",()=>console.log("scissors"));
